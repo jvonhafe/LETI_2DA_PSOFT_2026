5c45aa8e77da6232b1f82ab9ee16f5834ba85838
pcvaz <1240908@isep.ipp.pt>
2026-06-13 20:01:04 +0100

    commit final para entrega de hj orh«ganização de pastas .json unificado melhoras no airport e dm finais

--- demo/src/main/java/com/aisafe/controller/AuthController.java
+++ demo/src/main/java/com/aisafe/controller/AuthController.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @Tag(name = "Authentication", description = "Autenticação JWT")
 public class AuthController {
 
